1e581c8af2344823e7dd9170b0a9757f22551bbc
ErinCleaver-GRCC <erincleaver@email.grcc.edu>
2021-05-28 07:20:56 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 13-roman-to-integer
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/13-roman-to-integer)
---
+++ index.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+
+/*
+Problem is to find roman numerals and change them to a Interger
+The remote numerls are 
+
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+
+Limitations:
+1 <= s.length <= 15
+s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+*/
+
+// 
+
+var romanToInt = function(s) {
+    value = 0;
+    
+
+
+
+    return value;
+};
+
+console.log(`The input is III and the output should be 3 for the roman to int function: ${romanToInt("III")}`)
+
+console.log(`The input is IV and the output should be 4 for the roman to int function: ${romanToInt("IV")}`)
+
+console.log(`The input is IX and the output should be 9 for the roman to int function: ${romanToInt("IX")}`)
+
+console.log(`The input is MCMXCIV and the output should be 1994 for the roman to int function: ${romanToInt("MCMXCIV")}`)
